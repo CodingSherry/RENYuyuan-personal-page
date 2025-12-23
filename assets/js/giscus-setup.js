beb06518a9ea75103fea43ae2e679a3c094e761b
CodingSherry <12411321@mail.sustech.edu.cn>
2025-12-23 15:03:26 +0000

    Deploying to gh-pages from @ CodingSherry/CodingSherry.github.io@939c259ce01151d38702f46a552274d0d3260f2e 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "CodingSherry/RENYuyuan-personal-page",
+    "data-repo": "CodingSherry/CodingSherry.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
